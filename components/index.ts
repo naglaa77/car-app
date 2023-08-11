2b87ad3977bb11df66479a0f65049b88fde891c8
naglaa77 <naglaafouz4@gmail.com>
2023-08-11 20:34:16 +0200

    Feat(hero):Add hero section

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+// i use this fill as a single file that's going export all of our components
+import Hero from './Hero'
+import CustomButton from "@/components/CustomButton";
+export {
+    Hero,
+    CustomButton,
+}
