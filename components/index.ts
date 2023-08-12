4d1c0ec708583a393f0188d06f830e826532c579
naglaa77 <naglaafouz4@gmail.com>
2023-08-12 14:34:37 +0200

    Feat(Home):Add manufacturer filter

--- components/index.ts
+++ components/index.ts
@@ -3,9 +3,15 @@ import Hero from './Hero'
 import CustomButton from "./CustomButton";
 import Navbar   from './Navbar'
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from './CustomFilter'
+import SearchManufacturer from './SearchManufacturer' // can also write like that => import {SearchManufacturer} from './'
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
