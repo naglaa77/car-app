ed3ca7183b31636f9e934f4198c0b358907e1596
naglaa77 <naglaafouz4@gmail.com>
2023-08-16 15:28:35 +0200

    Feat(Home):Search button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,16 @@
 'use client'
 import {useState} from "react";
 import SearchManufacturer from "./SearchManufacturer";
+import Image from "next/image";
 
+const SearchButton =({otherClasses}:{otherClasses:string}) =>(
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+        <Image src="/magnifying-glass.svg" alt="magnifying-glass" width={40} height={40} className="object-contain"/>
+    </button>
+)
 const SearchBar = () =>{
     const [manufacturer,setManufacturer] = useState('')
+    const [model, setModel] = useState('')
     const handleSearch =() =>{
 
     }
@@ -14,6 +21,11 @@ const SearchBar = () =>{
                     manufacturer={manufacturer}
                     setManufacturer={setManufacturer}
                 />
+                <SearchButton otherClasses="sm:hidden"/>
+            </div>
+            <div className="searchbar__item">
+                <Image src="/model-icon.png" alt="car model" width={25} height={25} className="absolute w-[20px] h-[20px] ml-4"/>
+                <input type="text" name="model" value={model}/>
             </div>
         </form>
     )
